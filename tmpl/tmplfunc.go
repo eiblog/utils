68cafc771ee1c8ee06f268a64aeed00906770c03
deepzz0 <deepzz.qi@gmail.com>
2017-07-08 14:44:01 +0800

    add tmpl func: base64img

--- tmpl/tmplfunc.go
+++ tmpl/tmplfunc.go
@@ -2,7 +2,11 @@
 package tmpl
 
 import (
+	"encoding/base64"
 	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -14,6 +18,7 @@ func init() {
 	TplFuncMap["str2html"] = Str2html
 	TplFuncMap["join"] = StringsJoin
 	TplFuncMap["isnotzero"] = IsNotZero
+	TplFuncMap["base64img"] = Base64Img
 }
 
 func Str2html(raw string) template.HTML {
@@ -21,9 +26,8 @@ func Str2html(raw string) template.HTML {
 }
 
 // DateFormat takes a time and a layout string and returns a string with the formatted date. Used by the template parser as "dateformat"
-func DateFormat(t time.Time, layout string) (datestring string) {
-	datestring = t.Format(layout)
-	return
+func DateFormat(t time.Time, layout string) string {
+	return t.Format(layout)
 }
 
 func StringsJoin(a []string, sep string) string {
@@ -32,4 +36,21 @@ func StringsJoin(a []string, sep string) string {
 
 func IsNotZero(t time.Time) bool {
 	return !t.IsZero()
+}
+
+func Base64Img(domain, avatar string) string {
+	resp, err := http.Get("https://" + domain + "/static/img/" + avatar)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+
+	return "data:" + resp.Header.Get("content-type") + ";base64," + base64.StdEncoding.EncodeToString(data)
 }
